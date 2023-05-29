f14c311e6c02de7ff82688511d6318fc9d35645c
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-05-29 14:40:15 +0530

    while and do-while loop concept

---
+++ while-do-while.js
@@ -0,0 +1,16 @@
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let i = 0;
+while (i < n) {
+  console.log(i);
+  i++;
+}
+
+// do-while loop
+let x = prompt("Enter the value of x");
+x = Number.parseInt(x);
+let j = 10;
+do {
+  console.log(j);
+  j++;
+} while (j < n);
