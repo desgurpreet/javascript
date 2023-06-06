83b08f83244bc1802b1abc06d3f1da2477fdb567
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-06-06 13:00:46 +0530

    using inner and outet html in js

---
+++ inner and outer html/js/inner.js
@@ -0,0 +1,5 @@
+console.log(first.innerHTML); //provide inner content of first tag
+console.log(first.outerHTML); //provide first tag with content
+console.log((first.innerHTML = "<i>Hey I am garry</i>")); //change first html tag content
+console.log((first.outerHTML = "<div><i>Hey I am garry</i></div>")); //change first html tag
+console.log(document.body.textContent); //get all text content write in html file
