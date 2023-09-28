a968afc01016c569714412983df3fd422fee5a10
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-09-28 15:57:23 +0530

    .then( ) and .catch( ) concept

--- promise/js/promise.js
+++ promise/js/promise.js
@@ -8,3 +8,26 @@ setTimeout(function () {
 }, 3000);
 console.log("My name is " + "Hello three");
 console.log(promise);
+//promise (.then() and .catch())
+let p1 = new Promise((resolve, reject) => {
+  console.log("promise is pending");
+  setTimeout(() => {
+    console.log("promise is resolve");
+    resolve(true);
+  }, 3000);
+});
+let p2 = new Promise((resolve, reject) => {
+  console.log("promise is pending");
+  setTimeout(() => {
+    console.log("promise is reject");
+    reject(false);
+  }, 5000);
+});
+p1.then((value) => {
+  //then for getting value
+  console.log(value);
+});
+p2.catch((value) => {
+  //catch for getting error
+  console.log(value);
+});
