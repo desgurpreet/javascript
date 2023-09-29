7ed04d85c678187f76abbef62ac8cbc628ac8ac9
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-09-29 09:15:21 +0530

    promise chaining by .then( )

--- promise/js/promise.js
+++ promise/js/promise.js
@@ -1,33 +1,23 @@
-let promise = new Promise(function (resolve, reject) {
-  alert("Hello");
-  resolve(56);
-});
-console.log("Hello One");
-setTimeout(function () {
-  console.log("Hello two");
-}, 3000);
-console.log("My name is " + "Hello three");
-console.log(promise);
-//promise (.then() and .catch())
-let p1 = new Promise((resolve, reject) => {
-  console.log("promise is pending");
+//promise chaining .then() calls
+let a1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("promise is resolve");
-    resolve(true);
-  }, 3000);
+    console.log("resolve");
+    resolve(20);
+  }, 2000);
 });
-let p2 = new Promise((resolve, reject) => {
-  console.log("promise is pending");
-  setTimeout(() => {
-    console.log("promise is reject");
-    reject(false);
-  }, 5000);
-});
-p1.then((value) => {
-  //then for getting value
+a1.then((value) => {
   console.log(value);
-});
-p2.catch((value) => {
-  //catch for getting error
-  console.log(value);
-});
+  let a2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("promise 2");
+    }, 5000);
+  });
+  return a2;
+})
+  .then((value) => {
+    console.log(value);
+    return 5;
+  })
+  .then((value) => {
+    console.log(value);
+  });
