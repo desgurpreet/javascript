e2f1f56eb87c438ab4355fae781d6cfab92fbe83
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-09-29 10:58:39 +0530

    promise api methods

--- promise/js/promise.js
+++ promise/js/promise.js
@@ -1,23 +1,36 @@
-//promise chaining .then() calls
-let a1 = new Promise((resolve, reject) => {
+// promise API methods
+let b1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("resolve");
-    resolve(20);
+    resolve("value 1");
+  }, 1000);
+});
+let b2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("value 2");
   }, 2000);
 });
-a1.then((value) => {
+let b3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("value 3");
+  }, 3000);
+});
+let promise_all = Promise.all([b1, b2, b3]);
+promise_all.then((value) => {
+  console.log("promise all");
   console.log(value);
-  let a2 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("promise 2");
-    }, 5000);
-  });
-  return a2;
-})
-  .then((value) => {
-    console.log(value);
-    return 5;
-  })
-  .then((value) => {
-    console.log(value);
-  });
+});
+let promise_settled = Promise.allSettled([b1, b2, b3]);
+promise_settled.then((value) => {
+  console.log("promise allSettled");
+  console.log(value);
+});
+let promise_race = Promise.race([b1, b2, b3]);
+promise_race.then((value) => {
+  console.log("promise race");
+  console.log(value);
+});
+let promise_re = Promise.resolve(7);
+promise_re.then((value) => {
+  console.log("promise resolved");
+  console.log(value);
+});
