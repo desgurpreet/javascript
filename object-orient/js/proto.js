e10f73a81ae2412248a236f10a8b224c4316e415
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-10-05 16:19:00 +0530

    class and objects

--- object-orient/js/proto.js
+++ object-orient/js/proto.js
@@ -19,3 +19,25 @@ p.__proto__ = {
 a.__proto__ = p;
 a.run();
 console.log(a.name2);
+
+// class and object
+
+class form {
+  submit() {
+    alert(this.name + "your form is successfully submitted");
+  }
+  cancel() {
+    alert(this.name + " form is cancelled");
+  }
+  fill(givenname) {
+    this.name = givenname;
+  }
+}
+
+let gurpreet = new form();
+gurpreet.fill("Gurpreet Singh");
+let hardeep = new form();
+hardeep.fill("Hardeep Singh");
+
+gurpreet.submit();
+hardeep.cancel();
