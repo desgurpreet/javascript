710895dc16e6da2d764c6d5713f40f149c206617
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-10-09 11:38:41 +0530

    spread operator

--- chapter_12/js/ch12.js
+++ chapter_12/js/ch12.js
@@ -16,4 +16,19 @@ let a = () => {
   console.log(d);
 })();
 
+// destructuring
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let [a1, b1, ...rest] = arr;
+console.log(a1, b1, rest);
+let { i, j } = { i: 1, b: 5 };
+console.log(a, b);
 
+// spread operator
+let arr1 = [3, 4, 5];
+let obj1 = { ...arr1 };
+console.log(obj1);
+
+function sum(v1, v2, v3) {
+  return v1 + v2 + v3;
+}
+console.log(sum(...arr1));
