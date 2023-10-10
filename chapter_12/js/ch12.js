db050496aa055ffca7c70fff4d0ce746e625a80e
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-10-10 11:01:13 +0530

    closures

--- chapter_12/js/ch12.js
+++ chapter_12/js/ch12.js
@@ -20,8 +20,8 @@ let a = () => {
 let arr = [1, 2, 3, 4, 5, 6, 7];
 let [a1, b1, ...rest] = arr;
 console.log(a1, b1, rest);
-let { i, j } = { i: 1, b: 5 };
-console.log(a, b);
+let { i, j } = { i: 1, j: 5 };
+console.log(i, j);
 
 // spread operator
 let arr1 = [3, 4, 5];
@@ -32,3 +32,25 @@ function sum(v1, v2, v3) {
   return v1 + v2 + v3;
 }
 console.log(sum(...arr1));
+
+// closures
+function returnFunc() {
+  const x = () => {
+    let e = 1;
+    console.log(e);
+    const y = () => {
+      //   let e = 2;
+      console.log(e);
+      const z = () => {
+        let e = 3;
+        console.log(e);
+      };
+      z();
+    };
+    e = 234;
+    y();
+  };
+  return x;
+}
+let f = returnFunc();
+f();
