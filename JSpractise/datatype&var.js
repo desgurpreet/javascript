790a2dca596d9dcb03ba21733b02dbd3728c140a
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-11-28 10:35:45 +0530

    datatypes

---
+++ JSpractise/datatype&var.js
@@ -0,0 +1,19 @@
+let score = 10;
+score = 11;
+console.log(score);
+
+// datatype
+const name = "preet"; //string
+const age = 20; //number
+const rating = 4.5; //number
+const isCool = true; //boolean
+const x = null;
+const y = undefined;
+let z;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
