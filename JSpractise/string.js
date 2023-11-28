f95640fc788733f5571865452be3996f0f684ab1
des.gurpreetsingh <des.gurpreetsingh@gmail.com>
2023-11-28 11:13:37 +0530

    string and method

---
+++ JSpractise/string.js
@@ -0,0 +1,10 @@
+const name = "gurpreet";
+const last = "singh";
+console.log(`My name is ${name} ${last}`);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.split()); //return string into array form
+console.log(name.split("")); //split by letter
+
+const s = "name, age, skills, address"; //convert into array format
+console.log(name.split(", "));
